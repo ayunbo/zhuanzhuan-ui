2a4e0225e7aa0fb18a5ac9e3fafe3cca2bc7c400
yunbo <pyb20041021@126.com>
2026-05-24 19:25:34 +0800

    移除未使用的Vant全局样式

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import './assets/main.css'
 import 'element-plus/dist/index.css'
+import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -8,12 +8,9 @@ import ElementPlus from 'element-plus'
 import App from './App.vue'
 import router from './router'
 
-import Vant from 'vant'
-import 'vant/lib/index.css'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus)
-app.use(Vant)
 app.mount('#app')
